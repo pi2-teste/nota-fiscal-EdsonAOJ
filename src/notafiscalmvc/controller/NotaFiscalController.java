3a1b7af363dea3c624e5abaca6d7bdae15578748
EDSON ALVES DE OLIVEIRA JUNIOR <edson.aojunior3@senacsp.edu.br>
2019-10-24 11:07:18 -0300

    ADO2

---
+++ src/notafiscalmvc/controller/NotaFiscalController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.sp.notafiscalmvc.controller;
+
+import br.senac.sp.notafiscalmvc.DAO.NotaFiscalDAO;
+import br.senac.sp.notafiscalmvc.model.NotaFiscal;
+
+/**
+ *
+ * @author Edson
+ */
+public class NotaFiscalController {
+
+    public static boolean salvar(int numeroNota, String descricaodaNota, double valorNota) {
+
+        NotaFiscal nota = new NotaFiscal();
+        System.out.println("agora estou no controle");
+        nota.setValNota(valorNota);
+        nota.setDescricaoNota(descricaodaNota);
+        nota.setNumNota(numeroNota);
+        NotaFiscalDAO dao = new NotaFiscalDAO();
+        dao.addNota(nota);
+        return true;
+    }
+
+    public boolean getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
